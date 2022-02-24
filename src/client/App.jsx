9992c5879328022daf1708f3d01ba0c228660155
Theunis Knoetze <theunis.knoetze@gmail.com>
2022-02-24 18:31:22 +0300

    start Home page

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -2,12 +2,14 @@ import './App.css';
 
 import Login from './components/Login';
 import Register from './components/Registration';
+import Home from './components/Home';
 
 export default function App() {
   return (
     <div className="App">
-      <Login />
-      <Register />
+      {/* <Login />
+      <Register /> */}
+      <Home />
     </div>
   );
 }
---
+++ src/client/components/Home.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+const Home = () => {
+    return (
+        <div className="homePage">
+            <header className="homeHeader">
+
+            </header>
+            <section className="mainContainer">
+                <div className="postContainer">
+
+                </div>
+                <div className="ratingContainer">
+
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default Home;
