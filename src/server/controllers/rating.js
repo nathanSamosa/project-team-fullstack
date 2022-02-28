72dcbb582b3bc3b0597fa939e4b0e2b61c72acea
nathanSamosa <nathanthesamosa@gmail.com>
2022-02-28 11:23:08 +0000

    add seed ratings and displays in pokedex

--- src/server/controllers/rating.js
+++ src/server/controllers/rating.js
@@ -1,6 +1,9 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
+const pokeNumber = 151;
+const getRandom = (min, max) => Math.floor(Math.random() * (max - min) + min);
+
 const getRatings = async (req, res) => {
     const ratings = await prisma.rating.findMany({})
     res.json({ data: ratings })
@@ -28,7 +31,26 @@ const postRating = async (req, res) => {
     res.json({ data: createdRating })
 }
 
+const seedRating = async (req, res) => {
+
+    for (let i = 1; i <= pokeNumber; i++) {
+        const pokemonId = i;
+        const seededRating = await prisma.rating.create({
+            data: {
+                profileId: 1,
+                pokemonId: pokemonId,
+                strength: getRandom(1, 6),
+                companion: getRandom(1, 6),
+                design: getRandom(1, 6)
+            }
+        })
+        console.log(seededRating)
+    }
+    res.json({ data: "ratings seeded" })
+}
+
 module.exports = {
     getRatings,
-    postRating
+    postRating,
+    seedRating
 }
